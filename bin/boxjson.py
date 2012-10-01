d628b6bc9927784e760c588e66adfd2596927364
Dragon Dave <dragon@scraperwiki.com>
2012-10-01 14:17:34 +0000

    boxjson

---
+++ bin/boxjson.py
@@ -0,0 +1,28 @@
+import json
+
+jsonfile='/home/scraperwiki.json'
+
+def getjson():
+    str_data=open(jsonfile,'r').read()
+    return json.loads(str_data)  
+
+def savejson(data):
+    str_data=json.dumps(data)
+    fh=open(jsonfile, 'w')
+    fh.write(str_data)
+    fh.close()
+
+class boxjson():
+    def parse(self, *args):
+        if len(args)==1:
+            return getjson()[args[0]]
+        elif len(args)==2:
+            newjson=getjson()
+            newjson[args[0]]=args[1]
+            savejson(newjson)
+        else:
+            return getjson()
+
+    def __getattr__(self, *args, **kwargs):
+        return self.parse
+
